5ce8140a3897f250759aee630f6aef4f8471eff9
Joao.Morais <97574443+jpmmat@users.noreply.github.com>
2025-11-08 11:58:11 +0000

    objeto

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const person = {
+    nome: 'Joao',
+    age: 28,
+    Weight: 86.5,
+    isAdmin: true,
+}
+console.log('$person.nome) tem $(person.age) anos e pesa $(person.weith) kg');
+const animals = {
+    'Lion',
+    'Monkey',
+    person,
+    {
+        nome: 'cat',
+        age: 2,
+        weight: 5,    }
+}
